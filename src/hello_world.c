3234169684f56271cd799104e268b5bdf3fdec35
Andrew Shildmyer <ashildmy@purdue.edu>
2014-10-27 17:17:43 -0900

    Up and Down Buttons Increment Shots

--- src/hello_world.c
+++ src/hello_world.c
@@ -1,38 +1,47 @@
 #include <pebble.h>
 
+int shots;
+
 static Window *window;
 static TextLayer *text_layer;
 
-static void window_load(Window *window){
-
+void up_single_click_handler(ClickRecognizerRef recognizer, void *context){
+  shots++;  
+  APP_LOG(0, "%i\n", shots);
 }
 
-static void window_unload(Window *window){
+void down_single_click_handler(ClickRecognizerRef recognizer, void *context){
+  if(shots > 0){
+    shots--;  
+  }
+  APP_LOG(0, "%i\n", shots);
+}
 
+void config_provider(Window *window){
+  window_single_click_subscribe(BUTTON_ID_UP, up_single_click_handler);
+  
+  window_single_click_subscribe(BUTTON_ID_DOWN, down_single_click_handler);
 }
 
 static void init(void) {
-	window = window_create();
-  	window_stack_push(window, true);
+  const bool animate = true;
+  shots = 0;
+  window = window_create();
+  window_stack_push(window, animate);
 
-	Layer *window_layer = window_get_root_layer(window);	
-	GRect bounds = layer_get_frame(window_layer);
-	text_layer = text_layer_create((GRect(0, 0, 40, 40)));
-	text_layer_set_text(text_layer, "Accuracy Counter");
-	text_layer_set_text_color(text_layer, GColorWhite);
-	text_layer_set_background_color(text_layer, GColorBlack);
-	text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
-	layer_add_child(window_layer, text_layer_get_layer(text_layer));
+  window_set_click_config_provider(window, (ClickConfigProvider) config_provider);
 
-//  window_set_window_handlers(window, (WindowHandlers) {
- //   .load = window_load,
-  //  .unload = window_unload,
- // });
-  //const bool animated = true;
+  Layer *window_layer = window_get_root_layer(window);	
+  text_layer = text_layer_create((GRect(0, 0, 144, 25)));
+  text_layer_set_text(text_layer, "Accuracy Counter");
+  text_layer_set_text_color(text_layer, GColorWhite);
+  text_layer_set_background_color(text_layer, GColorBlack);
+  text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
+  layer_add_child(window_layer, text_layer_get_layer(text_layer));
 }
 
 static void deinit(void) {
-	text_layer_destroy(text_layer);
+  text_layer_destroy(text_layer);
   window_destroy(window);
 }
 
