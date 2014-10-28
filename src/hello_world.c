1000a03170102f9d53cf42089bcc4dd20da977f5
Kirby Kohlmorgen <kirby@purdue.edu>
2014-10-27 22:18:00 -0400

    Add count text

--- src/hello_world.c
+++ src/hello_world.c
@@ -1,7 +1,7 @@
 #include <pebble.h>
 
 static Window *window;
-static TextLayer *text_layer;
+static TextLayer *count_layer;
 
 static void window_load(Window *window){
 
@@ -11,28 +11,27 @@ static void window_unload(Window *window){
 
 }
 
+static void create_count_text(Layer* window_layer) {
+  count_layer = text_layer_create((GRect(0, 40, 144, 100)));
+  text_layer_set_text(count_layer, "0");
+  text_layer_set_text_color(count_layer, GColorBlack);
+  text_layer_set_font(count_layer,  fonts_get_system_font(FONT_KEY_ROBOTO_BOLD_SUBSET_49));
+  text_layer_set_text_alignment(count_layer, GTextAlignmentCenter);
+  layer_add_child(window_layer, text_layer_get_layer(count_layer));
+}
+
 static void init(void) {
-	window = window_create();
-  	window_stack_push(window, true);
+  window = window_create();
+  window_stack_push(window, true);
 
-	Layer *window_layer = window_get_root_layer(window);	
-	GRect bounds = layer_get_frame(window_layer);
-	text_layer = text_layer_create((GRect(0, 0, 40, 40)));
-	text_layer_set_text(text_layer, "Accuracy Counter");
-	text_layer_set_text_color(text_layer, GColorWhite);
-	text_layer_set_background_color(text_layer, GColorBlack);
-	text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
-	layer_add_child(window_layer, text_layer_get_layer(text_layer));
+  Layer *window_layer = window_get_root_layer(window);	
+  GRect bounds = layer_get_frame(window_layer);
 
-//  window_set_window_handlers(window, (WindowHandlers) {
- //   .load = window_load,
-  //  .unload = window_unload,
- // });
-  //const bool animated = true;
+  create_count_text(window_layer);
 }
 
 static void deinit(void) {
-	text_layer_destroy(text_layer);
+  text_layer_destroy(count_layer);
   window_destroy(window);
 }
 
